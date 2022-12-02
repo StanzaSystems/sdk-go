faefad1e9bbe88e7d186873ed83ee789d5d6e34b
Matthew Girard <msg@sre.is>
2022-12-02 18:03:07 -0500

    remove env from fiberstanza

--- adapters/fiberstanza/config.go
+++ adapters/fiberstanza/config.go
@@ -3,5 +3,4 @@ package fiberstanza
 // Config defines the config for fiberstanza middleware.
 type Config struct {
 	ResourceName string `json:"resourceName"` // optional (but required if you want to protect multiple resources)
-	Environment  string `json:"environment"`  // defaults to "dev" if unset
 }
