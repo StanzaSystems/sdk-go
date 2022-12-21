09f9eee3ba3b5620a8b4e8d1d0275d75b00df59d
Matthew Girard <msg@sre.is>
2022-12-21 12:49:00 -0500

    moving otel into stanza handler

--- adapters/fiberstanza/fiberstanza.go
+++ adapters/fiberstanza/fiberstanza.go
@@ -10,22 +10,6 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/valyala/fasthttp/fasthttpadaptor"
-	"go.opentelemetry.io/contrib"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/metric"
-	"go.opentelemetry.io/otel/metric/global"
-	"go.opentelemetry.io/otel/metric/instrument"
-	"go.opentelemetry.io/otel/metric/unit"
-	"go.opentelemetry.io/otel/propagation"
-)
-
-const (
-	instrumentationName = "github.com/StanzaSystems/sdk-go/adapters/fiberstanza"
-
-	metricNameHttpServerDuration       = "http.server.duration"
-	metricNameHttpServerRequestSize    = "http.server.request.size"
-	metricNameHttpServerResponseSize   = "http.server.response.size"
-	metricNameHttpServerActiveRequests = "http.server.active_requests"
 )
 
 // New creates a new fiberstanza middleware fiber.Handler
@@ -34,73 +18,12 @@ func New(config Config) fiber.Handler {
 		logging.Error(err, "failed to register new resource")
 	}
 
-	meter := global.Meter(
-		instrumentationName,
-		metric.WithInstrumentationVersion(contrib.SemVersion()),
-	)
-	httpServerDuration, err := meter.SyncFloat64().Histogram(
-		metricNameHttpServerDuration,
-		instrument.WithUnit(unit.Milliseconds),
-		instrument.WithDescription("measures the duration inbound HTTP requests"))
-	if err != nil {
-		otel.Handle(err)
-	}
-	httpServerRequestSize, err := meter.SyncInt64().Histogram(
-		metricNameHttpServerRequestSize,
-		instrument.WithUnit(unit.Bytes),
-		instrument.WithDescription("measures the size of HTTP request messages"))
-	if err != nil {
-		otel.Handle(err)
-	}
-	httpServerResponseSize, err := meter.SyncInt64().Histogram(
-		metricNameHttpServerResponseSize,
-		instrument.WithUnit(unit.Bytes),
-		instrument.WithDescription("measures the size of HTTP response messages"))
-	if err != nil {
-		otel.Handle(err)
-	}
-	httpServerActiveRequests, err := meter.SyncInt64().UpDownCounter(
-		metricNameHttpServerActiveRequests,
-		instrument.WithUnit(unit.Dimensionless),
-		instrument.WithDescription("measures the number of concurrent HTTP requests that are currently in-flight"))
-	if err != nil {
-		otel.Handle(err)
-	}
-
 	return func(c *fiber.Ctx) error {
-		savedCtx, cancel := context.WithCancel(c.UserContext())
-
-		start := time.Now()
-		httpServerActiveRequests.Add(savedCtx, 1)
-		defer func() {
-			httpServerDuration.Record(savedCtx, float64(time.Since(start).Microseconds())/1000)
-			httpServerRequestSize.Record(savedCtx, int64(len(c.Request().Body())))
-			httpServerResponseSize.Record(savedCtx, int64(len(c.Response().Body())))
-			httpServerActiveRequests.Add(savedCtx, -1)
-			c.SetUserContext(savedCtx)
-			cancel()
-		}()
-
-		reqHeader := make(http.Header)
-		c.Request().Header.VisitAll(func(k, v []byte) {
-			reqHeader.Add(string(k), string(v))
-		})
-
-		prop := otel.GetTextMapPropagator()
-		ctx := prop.Extract(savedCtx, propagation.HeaderCarrier(reqHeader))
-
-		// TODO: Start the trace here
-		// spanName := utils.CopyString(c.Path())
-		// ctx, span := tracer.Start(ctx, spanName, opts...)
-		// defer span.End()
-
-		// pass the span through userContext
-		c.SetUserContext(ctx)
-
 		// TODO(msg): implement HttpInboundHandler as fasthttp handler instead of converting to net/http?
 		var req http.Request
 		if err := fasthttpadaptor.ConvertRequest(c.Context(), &req, true); err != nil {
-			return err
+			logging.Error(err, "failed to convert request")
+			return c.Next() // log error and fail open
 		}
 		if status := stanza.HttpInboundHandler(config.ResourceName, &req); status != http.StatusOK {
 			return c.SendStatus(status)
--- adapters/fiberstanza/go.mod
+++ adapters/fiberstanza/go.mod
@@ -6,9 +6,6 @@ require (
 	github.com/StanzaSystems/sdk-go v0.0.0-00010101000000-000000000000
 	github.com/gofiber/fiber/v2 v2.40.1
 	github.com/valyala/fasthttp v1.41.0
-	go.opentelemetry.io/contrib v1.12.0
-	go.opentelemetry.io/otel v1.11.2
-	go.opentelemetry.io/otel/metric v0.34.0
 )
 
 require (
@@ -18,12 +15,13 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/fsnotify/fsnotify v1.4.7 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.2.4 // indirect
-	github.com/golang/protobuf v1.4.3 // indirect
-	github.com/google/uuid v1.1.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.1.2 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
@@ -43,15 +41,21 @@ require (
 	github.com/tklauser/numcpus v0.2.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.37.0 // indirect
+	go.opentelemetry.io/otel v1.11.2 // indirect
+	go.opentelemetry.io/otel/exporters/stdout/stdoutmetric v0.34.0 // indirect
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.11.2 // indirect
+	go.opentelemetry.io/otel/metric v0.34.0 // indirect
 	go.opentelemetry.io/otel/sdk v1.11.2 // indirect
+	go.opentelemetry.io/otel/sdk/metric v0.34.0 // indirect
 	go.opentelemetry.io/otel/trace v1.11.2 // indirect
 	go.uber.org/atomic v1.6.0 // indirect
 	go.uber.org/multierr v1.5.0 // indirect
 	go.uber.org/zap v1.13.0 // indirect
-	golang.org/x/lint v0.0.0-20190930215403-16217165b5de // indirect
+	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
 	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
-	golang.org/x/tools v0.0.0-20200103221440-774c71fcf114 // indirect
-	google.golang.org/protobuf v1.23.0 // indirect
+	golang.org/x/tools v0.1.12 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
