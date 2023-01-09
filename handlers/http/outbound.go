2f43b62fdf45954bfa9d769f5103ab3e169315ea
Matthew Girard <msg@sre.is>
2023-01-09 19:54:55 +0000

    add stanza events to otel span

--- handlers/http/outbound.go
+++ handlers/http/outbound.go
@@ -1,3 +1,6 @@
 package http
 
 // TODO: Implement outbound/client http meters and handlers
+
+// Use otel contrib http wrappers?
+// https://github.com/open-telemetry/opentelemetry-go-contrib/blob/main/instrumentation/net/http/otelhttp/client.go
