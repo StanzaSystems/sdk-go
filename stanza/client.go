e2840d67ed7b7b70871826fcde338efc965b4b3e
Joseph Bironas <josebiro@sre.is>
2023-05-26 19:43:28 +0000

    Rebased to d23e8a6ec777, and removed connect client in favor of vanilla grpc.

--- stanza/client.go
+++ stanza/client.go
@@ -2,28 +2,19 @@ package stanza
 
 import (
 	"context"
-	"errors"
-	"net/http"
 
-	v1 "github.com/StanzaSystems/sdk-go/gen/go/stanza/hub/v1"
-	"github.com/StanzaSystems/sdk-go/gen/go/stanza/hub/v1/hubv1connect"
 	"github.com/StanzaSystems/sdk-go/logging"
-	"github.com/bufbuild/connect-go"
+	v1 "github.com/StanzaSystems/sdk-go/proto/stanza/hub/v1"
 )
 
 func GetBearerToken(host string, apiKey string) (string, error) {
-	client := hubv1connect.NewAuthServiceClient(
-		http.DefaultClient,
-		host,
-	)
-	req := connect.NewRequest(&v1.GetBearerTokenRequest{ApiKey: apiKey})
-	req.Header().Set("x-stanza-key", apiKey)
+	client := v1.NewAuthServiceClient()
+	req := &v1.GetBearerTokenRequest{ApiKey: apiKey}
 	res, err := client.GetBearerToken(context.Background(), req)
 	if err != nil {
 		logging.Error(err, "msg", "failed to to get bearer token")
 	}
-	token := res.Msg.GetBearerToken()
-	err = errors.New(res.Msg.GetErrorMessage())
+	token := res.GetBearerToken()
 
 	return token, err
 }
