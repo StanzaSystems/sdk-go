29d7b7a4ec2cb4338452789020577adc9ccf3a57
Joseph Bironas <josebiro@sre.is>
2023-05-26 19:52:02 +0000

    Fixing build/connection issues.

--- stanza/client.go
+++ stanza/client.go
@@ -5,10 +5,17 @@ import (
 
 	"github.com/StanzaSystems/sdk-go/logging"
 	v1 "github.com/StanzaSystems/sdk-go/proto/stanza/hub/v1"
+	"google.golang.org/grpc"
 )
 
 func GetBearerToken(host string, apiKey string) (string, error) {
-	client := v1.NewAuthServiceClient()
+	conn, err := grpc.Dial(host, grpc.WithInsecure())
+	if err != nil {
+		logging.Error(err, "msg", "failed to connect to stanza hub")
+	}
+	defer conn.Close()
+
+	client := v1.NewAuthServiceClient(conn)
 	req := &v1.GetBearerTokenRequest{ApiKey: apiKey}
 	res, err := client.GetBearerToken(context.Background(), req)
 	if err != nil {
