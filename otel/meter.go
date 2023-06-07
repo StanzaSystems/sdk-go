a6a12a6389fa1c40444afd25b8fee62082b81b4a
Matthew Girard <msg@sre.is>
2023-06-07 14:34:02 +0000

    better default OTEL init

--- otel/meter.go
+++ otel/meter.go
@@ -5,7 +5,6 @@ import (
 	"crypto/tls"
 	"fmt"
 	"os"
-	"time"
 
 	hubv1 "github.com/StanzaSystems/sdk-go/proto/stanza/hub/v1"
 
@@ -31,12 +30,28 @@ func initDebugMeter(res *resource.Resource) (*metric.MeterProvider, error) {
 
 func initGrpcMeter(ctx context.Context, res *resource.Resource, config *hubv1.MetricConfig, token string) (*metric.MeterProvider, error) {
 	opts := []otlpmetricgrpc.Option{
-		otlpmetricgrpc.WithRetry(otlpmetricgrpc.RetryConfig{
-			Enabled:         true,
-			InitialInterval: 5 * time.Second,
-			MaxInterval:     30 * time.Second,
-			MaxElapsedTime:  time.Minute,
-		}),
+		// WithRetry sets the retry policy for transient retryable errors that are
+		//   returned by the target collector endpoint.
+		//   Defaults to 5 seconds after receiving a retryable error and increase
+		//   exponentially after each error for no more than a total time of 1 minute.
+		// otlpmetricgrpc.WithRetry(otlpmetricgrpc.RetryConfig{
+		// 	Enabled:         true,
+		// 	InitialInterval: 5 * time.Second,
+		// 	MaxInterval:     30 * time.Second,
+		// 	MaxElapsedTime:  time.Minute,
+		// }),
+		//
+		// WithTimeout sets the max amount of time an Exporter will attempt an export.
+		//   This takes precedence over the retry settings defined above. Once this time
+		//   limit has been reached the export is abandoned and the metric data is dropped.
+		//   Defaults to 10 seconds.
+		// otlpmetricgrpc.WithTimeout(30 * time.Second),
+		//
+		// WithReconnectionPeriod sets the minimum amount of time between connection
+		//   attempts to the target endpoint.
+		// otlpmetricgrpc.WithReconnectionPeriod(1 * time.Minute),
+		//
+		otlpmetricgrpc.WithEndpoint(config.GetCollectorUrl()),
 		otlpmetricgrpc.WithHeaders(map[string]string{
 			"Authorization": "Bearer " + token,
 		}),
--- otel/tracer.go
+++ otel/tracer.go
@@ -5,7 +5,6 @@ import (
 	"crypto/tls"
 	"fmt"
 	"os"
-	"time"
 
 	hubv1 "github.com/StanzaSystems/sdk-go/proto/stanza/hub/v1"
 
@@ -24,8 +23,8 @@ func initDebugTracer(resource *resource.Resource, config *hubv1.TraceConfig) (*t
 		return nil, fmt.Errorf("creating stdout trace exporter: %w", err)
 	}
 
-	// ParentBased will enable sampling if the Parent sampled, otherwise use *default*
-	// ratio of 1/10 of a percent (can be changed via Hub or STANZA_DEFAULT_TRACE_RATIO)
+	// ParentBased will enable sampling if the Parent sampled, otherwise use the
+	// default sample rate given by Hub (which is 1/10th of 1% of requests).
 	// TODO: Handle trace sample rate overrides
 	sampleRate := float64(config.GetSampleRateDefault())
 
@@ -44,12 +43,28 @@ func initDebugTracer(resource *resource.Resource, config *hubv1.TraceConfig) (*t
 
 func initGrpcTracer(ctx context.Context, resource *resource.Resource, config *hubv1.TraceConfig, token string) (*trace.TracerProvider, error) {
 	opts := []otlptracegrpc.Option{
-		otlptracegrpc.WithRetry(otlptracegrpc.RetryConfig{
-			Enabled:         true,
-			InitialInterval: 5 * time.Second,
-			MaxInterval:     30 * time.Second,
-			MaxElapsedTime:  2 * time.Minute,
-		}),
+		// WithRetry sets the retry policy for transient retryable errors that may be
+		//   returned by the target collector endpoint when exporting a batch of spans.
+		//   Defaults to 5 seconds after receiving a retryable error and increase
+		//   exponentially after each error for no more than a total time of 1 minute.
+		// otlptracegrpc.WithRetry(otlptracegrpc.RetryConfig{
+		// 	Enabled:         true,
+		// 	InitialInterval: 5 * time.Second,
+		// 	MaxInterval:     30 * time.Second,
+		// 	MaxElapsedTime:  time.Minute,
+		// }),
+		//
+		// WithTimeout sets the max amount of time a client will attempt to export a
+		//   batch of spans. This takes precedence over any retry settings defined above,
+		//   once this time limit has been reached the export is abandoned and the batch
+		//   of spans is dropped. Defaults to 10 seconds.
+		// otlptracegrpc.WithTimeout(30 * time.Second),
+		//
+		// WithReconnectionPeriod sets the minimum amount of time between connection
+		//   attempts to the target endpoint.
+		// otlptracegrpc.WithReconnectionPeriod(30 * time.Second),
+		//
+		otlptracegrpc.WithEndpoint(config.GetCollectorUrl()),
 		otlptracegrpc.WithHeaders(map[string]string{
 			"Authorization": "Bearer " + token,
 		}),
@@ -68,8 +83,8 @@ func initGrpcTracer(ctx context.Context, resource *resource.Resource, config *hu
 		return nil, fmt.Errorf("creating OTLP trace exporter: %w", err)
 	}
 
-	// ParentBased will enable sampling if the Parent sampled, otherwise use *default*
-	// ratio of 1/10 of a percent (can be changed via Hub or STANZA_DEFAULT_TRACE_RATIO)
+	// ParentBased will enable sampling if the Parent sampled, otherwise use the
+	// default sample rate given by Hub (which is 1/10th of 1% of requests).
 	// TODO: Handle trace sample rate overrides
 	sampleRate := float64(config.GetSampleRateDefault())
 
