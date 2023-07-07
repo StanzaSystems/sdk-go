67f60b12aa8afa739bdd90aa9a03720f1e853099
Matthew Girard <114418397+sre-is-msg@users.noreply.github.com>
2023-07-07 12:00:13 -0400

    Propagate additional headers (#58)
    * propagate additional headers
    
    * use keys

--- keys/keys.go
+++ keys/keys.go
@@ -1,7 +1,16 @@
 package keys
 
-type outboundHeadersContextKey string
+type ContextKey string
+
+const (
+	StzBoost = "stz-boost"
+	StzFeat  = "stz-feat"
+)
 
 var (
-	OutboundHeadersKey = outboundHeadersContextKey("stanzaOutboundHeaders")
+	OutboundHeadersKey = ContextKey("stanza-outbound-headers")
+	UberctxStzBoostKey = ContextKey("uberctx-" + StzBoost)
+	UberctxStzFeatKey  = ContextKey("uberctx-" + StzFeat)
+	OtStzBoostKey      = ContextKey("ot-baggage-" + StzBoost)
+	OtStzFeatKey       = ContextKey("ot-baggage-" + StzFeat)
 )
